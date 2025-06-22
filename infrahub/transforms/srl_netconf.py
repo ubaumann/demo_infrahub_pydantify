055505ae677390c72172f48c7ca6bb92b842aa9e
ubaumann <github@m.ubaumann.ch>
2025-06-22 23:36:11 +0200

    Add routed subinterface payload

--- infrahub/transforms/srl_netconf.py
+++ infrahub/transforms/srl_netconf.py
@@ -6,6 +6,7 @@ from custom_helper.srl_netconf import main
 if TYPE_CHECKING:
     from custom_helper.protocols import (
         NetworkDevice,
+        NetworkInterface,
     )
 
 
@@ -14,4 +15,18 @@ class TransformSRLNetconf(InfrahubTransform):
 
     async def transform(self, data):
         device: NetworkDevice = self.nodes[0]
+
+        # Fetch all needed relations
+        await self.fetch(device)
+
         return await main(device)
+
+    async def fetch(self, device: "NetworkDevice"):
+        batch = await self.client.create_batch()
+        for interface_relation in device.interfaces.peers:
+            interface: "NetworkInterface" = interface_relation.peer
+            if interface.ip_address.id:
+                batch.add(task=interface.ip_address.fetch)
+
+        # Asynchronous List Comprehensions
+        [_ async for _ in batch.execute()]
--- setup/infrahub-worker/src/custom_helper/srl_netconf.py
+++ setup/infrahub-worker/src/custom_helper/srl_netconf.py
@@ -3,16 +3,41 @@ from typing import TYPE_CHECKING
 import pydantic_srlinux.models.interfaces as srl_if
 
 if TYPE_CHECKING:
+    from ipaddress import IPv4Interface
     from custom_helper.protocols import NetworkDevice, NetworkInterface
 
 
-def get_interface_payload(interface: "NetworkInterface") -> "srl_if.InterfaceListEntry":
-    return srl_if.InterfaceListEntry(name=interface.name.value)
+def routed_subinterface_payload(ip: "IPv4Interface") -> "srl_if.SubinterfaceListEntry":
+    return srl_if.SubinterfaceListEntry(
+        index=0,
+        ipv4=srl_if.Ipv4Container(address=[srl_if.AddressListEntry(ip_prefix=str(ip))]),
+    )
+
+
+def interface_payload(interface: "NetworkInterface") -> "srl_if.InterfaceListEntry":
+    admin_state = (
+        srl_if.EnumerationEnum.enable
+        if interface.status.value == "up"
+        else srl_if.EnumerationEnum.enable
+    )
+    subinterfaces: list[srl_if.SubinterfaceListEntry] | None = None
+    match interface.mode.value:
+        case "routed":
+            subinterfaces = [routed_subinterface_payload(interface.ip_address.peer)]
+        case _:
+            ...  # ToDo
+
+    return srl_if.InterfaceListEntry(
+        name=interface.name.value,
+        admin_state=admin_state,
+        description=interface.description.value,
+        subinterface=subinterfaces,
+    )
 
 
 async def main(device: "NetworkDevice") -> str:
     interface_models: list[srl_if.InterfaceListEntry] = []
     for interface in device.interfaces.peers:
-        interface_models.append(get_interface_payload(interface.peer))
+        interface_models.append(interface_payload(interface.peer))
     model = srl_if.Model(interface=interface_models)
     return model.model_dump_json(exclude_defaults=True, by_alias=True, indent=2)
