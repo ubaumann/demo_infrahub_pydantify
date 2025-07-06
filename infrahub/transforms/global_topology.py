f97ce9e513716614e9d2eb1fa5014be4695b7e34
ubaumann <github@m.ubaumann.ch>
2025-07-06 23:52:54 +0200

    Implement simple Graphviz artifact

--- infrahub/transforms/global_topology.py
+++ infrahub/transforms/global_topology.py
@@ -1,13 +1,21 @@
 from typing import Annotated, Any, Optional
 
+import httpx
 from infrahub_sdk.transforms import InfrahubTransform
 from pydantic import BaseModel, AliasPath, Field, BeforeValidator
 from jinja2 import Template
 
 
+
 # Remove the 2 digits at the end of the device name to get the role
 DeviceRole = Annotated[str, BeforeValidator(lambda x: x[:-2])]
 
+
+class Neighbor(BaseModel):
+    device_name: str = Field(validation_alias=AliasPath("device", "node", "name", "value"))
+    interface_name: str = Field(
+        None, validation_alias=AliasPath("name", "value")
+    )
 
 class Interface(BaseModel):
     name: str = Field(validation_alias=AliasPath("node", "name", "value"))
@@ -15,6 +23,9 @@ class Interface(BaseModel):
         None, validation_alias=AliasPath("node", "description", "value")
     )
     mode: str = Field(validation_alias=AliasPath("node", "mode", "value"))
+    neighbor: Optional[Neighbor] = Field(
+        None, validation_alias=AliasPath("node", "remote_interface", "node")
+    )
 
 
 class Device(BaseModel):
@@ -61,25 +72,43 @@ class TransformTopologyMarkdown(InfrahubTransform):
         return markdown
 
 
-class TransformTopologySVG(InfrahubTransform):
+class TransformTopologySVGGraphviz(InfrahubTransform):
     query = "GetNetworkDevices"
     graphiz_template = """
 graph {
  node [shape=box, style="rounded,filled"]
 {% for device in devices %}
- "{{ device }}"
-
-
-{% endfor %}
-
+ "{{ device.name }}"
+{%- endfor %}
+{% for endpoint1, endpoint2 in links %}
+ "{{ endpoint1[0] }}" -- "{{ endpoint2[0] }}" [label="{{ endpoint1[1] }} - {{ endpoint2[1] }}"]
+{%- endfor %}
 }
 """
 
     async def transform(self, data):
         m = Topology.model_validate(data)
 
+        links = []
+
+        for device in m.devices:
+            for interface in device.interfaces:
+                if interface.neighbor:
+                    endpoints = [(device.name, interface.name), (interface.neighbor.device_name,interface.neighbor.interface_name)]
+                    endpoints.sort()
+                    if endpoints not in links:
+                        links.append(endpoints)
+
         graphiz = Template(self.graphiz_template).render(
-            devices=[x.name for x in m.devices]
+            devices=m.devices,
+            links=links,
         )
-        return graphiz
+        try:
+            async with httpx.AsyncClient() as client:
+                response = await client.post("https://kroki.io/graphviz/svg/", data=graphiz)
+                response.raise_for_status()
+
+        except httpx.HTTPError as exc:
+            raise Exception(f"Failed to generate SVG: {exc}") from exc
+        return response.text
 
